135463dfd8604e84c547621a8b2ec7f37305b41a
D@RE <alavya.pandey@gmail.com>
2022-12-02 07:29:23 +0530

    Binary Tree

---
+++ binaryTreePythonList.py
@@ -0,0 +1,92 @@
+# Tree Using Python List
+
+class BinaryTree:
+    def __init__(self,size):
+        self.customList = size * [None]
+        self.lastUsedIndex = 0
+        self.maxSize = size 
+        
+    # insert in a binary Tree
+    def insertNode(self,value):              # Time Complexity = O(1) ;; Space Complexity = O(1)
+        if self.lastUsedIndex + 1 ==self.maxSize:
+            return "The Tree is Full"
+        self.customList[self.lastUsedIndex + 1 ]  = value
+        self.lastUsedIndex +=1 
+        return "The value has been inserted"
+    
+
+    # serach method for a binary tree    
+    def searchNode(self,nodeValue):              # Time Complexity = O(N) ;; Space Complexity = O(1)
+        for i in range (len(self.customList)):
+            if self.customList[i] == nodeValue:
+                return "Success"
+        return "Not Found" 
+    
+    
+# PreOrder Traversal
+    def preOrderTraversal(self,index):               # Time Complexity = O(N) ;; Space Complexity = O(N)
+        if index > self.lastUsedIndex :
+            return
+        print(self.customList[index])
+        self.preOrderTraversal(index * 2)
+        self.preOrderTraversal(index * 2 + 1)
+# InOrder Traversal
+    def inOrderTraversal(self,index):               # Time Complexity = O(N) ;; Space Complexity = O(N)
+        if index > self.lastUsedIndex :
+            return
+        self.inOrderTraversal(index * 2)
+        print(self.customList[index])
+        self.inOrderTraversal(index * 2 + 1)
+        
+# PostOrder Traversal
+    def postOrderTraversal(self,index):               # Time Complexity = O(N) ;; Space Complexity = O(N)
+        if index > self.lastUsedIndex :
+            return
+        self.postOrderTraversal(index * 2)
+        self.postOrderTraversal(index * 2 + 1)
+        print(self.customList[index])
+        
+        
+# level Order traversal
+    def levelOrderTraversal(self, index):             # Time Complexity = O(N) ;; Space Complexity = O(N)
+        for i in range (index,self.lastUsedIndex +1):
+            print(self.customList[i])
+            
+            
+# Delete a node from a binary Tree
+    def deleteNode(self,value):                 # Time Complexity = O(N) ;; Space Complexity = O(1)
+        if self.lastUsedIndex == 0:
+            return "There is no Node to delete"
+        for i in range (1,self.lastUsedIndex + 1):
+            if self.customList[i] == value:
+                self.customList[i] = self.customList[self.lastUsedIndex]
+                self.customList[self.lastUsedIndex]  = None
+                self.lastUsedIndex -= 1
+                return "The Node has been Deleted"
+            
+            
+# Delete Entire Tree
+    def deleteBT(self):  # Time Complexity = O(1) ;; Space Complexity = O(1)
+        self.customList = None
+        return "The Binary Tree Has Been Deleted Successfully"
+        
+            
+    
+        
+        
+        
+newBT = BinaryTree(8)  #>> creation of a tree in python list           # Time Complexity = O(1) ;; Space Complexity = O(N)      
+newBT.insertNode("Drinks")
+newBT.insertNode("Hot")
+newBT.insertNode("Cold")
+newBT.insertNode("Tea")
+newBT.insertNode("Coffee")
+# print(newBT.searchNode("Cold"))
+# newBT.preOrderTraversal(1)
+# newBT.inOrderTraversal(1)
+# newBT.postOrderTraversal(1)
+# print(newBT.deleteNode("Hot"))
+print(newBT.deleteBT())
+
+# newBT.levelOrderTraversal(1)
+
--- treeLinkedList.py
+++ treeLinkedList.py
@@ -21,8 +21,6 @@ leftChild.rightChild = coffee
 rightChild = TreeNode("Cold")
 newBT.leftChild = leftChild
 newBT.rightChild = rightChild
-
-
 # preorder Traversal
 def preOrderTraversal(rootNode):            # Time Complexity = O(N) ;; Space Complexity = O(N)
     if not rootNode:
@@ -85,4 +83,113 @@ def searchBT(rootNode,nodeValue):            # Time Complexity = O(N) ;; Space C
                 customQueue.enqueue(root.value.rightChild)
         return "Not Found"
 
-print(searchBT(newBT,"Cola"))          
+# print(searchBT(newBT,"Cola"))          
+# Insert in Binary Tree
+def insertNodeBT(rootNode, newNode):            # Time Complexity = O(N) ;; Space Complexity = O(N)
+    if not rootNode:
+        rootNode = newNode
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            else:
+                root.value.leftChild = newNode
+                return "Successfully inserted"
+            
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+            else:
+                root.value.rightChild = newNode
+                return "Successfully inserted"
+            
+# newNode = TreeNode("Cola")
+# print(insertNodeBT(newBT,newNode))
+# levelOrderTraversal(newBT)
+
+
+# get the deepest  node in a tree
+def getDeepestNode(rootNode):                # Time Complexity = O(N) ;; Space Complexity = O(N)
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if (root.value.leftChild is not None):
+                customQueue.enqueue(root.value.leftChild)
+            
+            if (root.value.rightChild is not None):
+                customQueue.enqueue(root.value.rightChild)
+        deepestNode = root.value
+        return deepestNode
+
+# deepestNode = getDeepestNode(newBT)
+# print(deepestNode.data)
+
+# delete the deepest Node in a tree
+def deleteDeepestNode(rootNode,dNode):               # Time Complexity = O(N) ;; Space Complexity = O(N)
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value is dNode:
+                root.value = None
+                return
+            if root.value.rightChild:
+                if root.value.rightChild is  dNode :
+                    root.value.rightChild = None
+                    return
+                else:
+                    customQueue.enqueue(root.value.rightChild)
+           
+            if root.value.leftChild:
+                if root.value.leftChild is  dNode :
+                    root.value.leftChild = None
+                    return
+                else:
+                    customQueue.enqueue(root.value.leftChild)
+                    
+                    
+                    
+def delteNode(rootNode,node):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.data == node:
+                dNode =getDeepestNode(rootNode)
+                root.value.data = dNode.data
+                deleteDeepestNode(rootNode,dNode)
+                return "The node has been  deleted Successfully"
+            
+            if (root.value.leftChild is not None):
+                customQueue.enqueue(root.value.leftChild)
+            
+            if (root.value.rightChild is not None):
+                customQueue.enqueue(root.value.rightChild)
+        return "Failed to Delete!!"
+    
+    
+def deleteBT(rootNode):          # Time Complexity = O(1) ;; Space Complexity = O(1)
+    rootNode.data = None
+    rootNode.leftChild = None
+    rootNode.rightChild = None
+    return "The Tree Has Been Deleted"
+    
+# newNode = getDeepestNode(newBT)
+# deleteDeepestNode(newBT,newNode)
+
+# delete a node 
+# delteNode(newBT,"Tea")
+# levelOrderTraversal(newBT)
+print(deleteBT(newBT))
