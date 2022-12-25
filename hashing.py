fdb21b7802e6007ba467a2751b504454e79dd5aa
D@RE <alavya.pandey@gmail.com>
2022-12-25 06:43:40 +0530

    Hashing

--- hashing.py
+++ hashing.py
@@ -1,2 +1,22 @@
 # this is for hashing 
 # this is kind of a algorithm instead of data structure
+# hashing for integers values  
+def mod(number,cellnumber):
+    return (number%cellnumber)
+
+# hashing for ASCII functions -->> Strings
+def modASCII(string,cellnumber):
+    total = 0 
+    for i in string:
+        total += ord(i)
+    return (total % cellnumber)
+
+
+print(modASCII)
+print(mod)
+
+
+'''
+this is a sudo code just for understanding how hashing works, but hashing is much complex in real word
+This is just a basic for undsertanding
+'''
