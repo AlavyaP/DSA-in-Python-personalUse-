fe3280b6e060575cbb9f3a0e50408483bcffe0b2
D@RE <alavya.pandey@gmail.com>
2022-12-27 07:11:00 +0530

    thr

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -1,3 +1,5 @@
+import math
+
 # bubble sort 
 def bubbleSort(customList):             #Time Complexity = O(N^2) || Space Complexity = O(1)
     for i in range (len(customList)-1):
@@ -27,7 +29,86 @@ def insertionSort(customList):     #Time Complexity = O(N^2) || Space Complexity
             j -= 1 
         customList[j+1] = key
         
-    print(customList)
+    # print (customList)  >> this is for the insertSort only, we use return for bucketSort to get the value after sorting the elements from each buckets
+    return (customList)
+
+def bucketSort(customList):                 #Time Complexity = O(N^2) || Space Complexity = O(N)
+    numberOfBuckets = round(math.sqrt(len(customList)))
+    maxValue = max(customList)
+    arr = []
+    
+    # entering the element is appropriate bucket
+    for i in range (numberOfBuckets):
+        arr.append([])
+    for j in customList:
+        index_b = math.ceil(j*numberOfBuckets/maxValue)
+        arr[index_b-1].append(j)
+        
+    # for sorting the elemnts in each buckets
+    for i in range (numberOfBuckets):
+        arr[i] = insertionSort(arr[i])
+        
+    
+    # bring back all the buckets together 
+    k = 0
+    for i in range (numberOfBuckets):
+        for j in range (len(arr[i])):
+            customList[k] = arr[i][j]
+            k += 1
+    return (customList)
+
+
+# merge sort  helper function
+def merge(customList, l, m, r):
+    n1 = m -l + 1 
+    n2 = r - m
+    
+    L = [0] * (n1)
+    R = [0] * (n2)
+    
+    for i in range(0,n1):
+        L[i] = customList[l+i]
+        
+    for j in range(0,n2):
+        R[j] = customList[m+1+j]  #
+        
+        # merge this array
+        
+    i = 0 
+    j = 0
+    k = l 
+    
+    while i < n1 and j < n2 :
+        if L[i] <= R[j]:
+            customList[k] = L[i]
+            i += 1
+        else:
+            customList[k] = R[j] 
+            j += 1
+        k += 1
+        
+        
+    while i < n1 :
+        customList[k] = L[i]
+        i += 1
+        k += 1
+        
+    while j < n2 :
+        customList[k] = R[j]
+        j += 1
+        k += 1
+        
+# Merge Sort 
+def mergeSort(customList, l, r):            #Time Complexity = O(NLogN) || Space Complexity = O(N)
+    # calling them recursively
+    if l < r :
+        m = (l + (r-1))//2
+        mergeSort(customList,l,m)
+        mergeSort(customList,m+1,r)
+        merge(customList,l,m,r)
+    return customList
+        
+         
             
             
     
@@ -35,4 +116,6 @@ def insertionSort(customList):     #Time Complexity = O(N^2) || Space Complexity
 cList = [9,8,7,6,5,4,3,2,1]
 # bubbleSort(cList)
 # selectionSort(cList)
-insertionSort(cList)
+# insertionSort(cList)
+# print(bucketSort(cList))
+print(mergeSort(cList,0,8))
