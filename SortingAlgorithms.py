daa6bd43f72686ae9a9591198b62fd14180f661b
D@RE <alavya.pandey@gmail.com>
2022-12-29 06:41:48 +0530

    Sorting

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -108,9 +108,67 @@ def mergeSort(customList, l, r):            #Time Complexity = O(NLogN) || Space
         merge(customList,l,m,r)
     return customList
         
-         
-            
-            
+# Quick Sort helper function
+def swap(my_list,index1,index2):
+    # temp = my_list[index1]
+    # my_list[index1] = my_list[index2]
+    # my_list[index2] = temp >> the same is done in below code 
+    my_list[index1],my_list[index2] = my_list[index2],  my_list[index1] 
+    
+    
+def pivot(my_list,pivot_index,end_index):
+    swap_index = pivot_index
+    for i in range (pivot_index+1,end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index +=1
+            swap(my_list,swap_index,i)
+    swap(my_list,pivot_index,swap_index)
+    return swap_index
+
+
+# Quick Sort 
+def quickSort_helper(my_list,left,right):
+    if left < right:
+        pivot_index = pivot(my_list, left,right)
+        quickSort_helper(my_list,left,pivot_index -1)
+        quickSort_helper(my_list,pivot_index+1,right)
+    return my_list
+
+def quickSort(my_list):    #Time Complexity = O(NLogN) || Space Complexity = O(N)
+    return quickSort_helper(my_list,0,len(my_list)-1)
+        
+# test run for pivot and swap 
+my_list = [3,5,0,6,2,1,4]
+# print(pivot(my_list,0,6))
+# print(my_list)
+# print(quickSort(my_list))
+
+
+# Heap Sort
+# Heapify method
+def heapify(customList,n,i):
+    smallest = i
+    l = 2*i +1
+    r = 2*i +2
+    if l < n and customList[l] < customList[smallest] :
+        smallest = l
+    # For right side of tree     
+    if r < n and customList[r] < customList[smallest] :
+        smallest = r
+        
+    if smallest != i :
+        customList[i], customList[smallest] = customList[smallest], customList[i]
+        heapify(customList,n,smallest)    
+        
+# heap Sort      
+def heapSort(customList):            #Time Complexity = O(NLogN) || Space Complexity = O(1)
+    n = len(customList)
+    for i in range (int(n/2)-1,-1,-1):
+        heapify(customList,n,i)
+    for i in range (n-1,0,-1):
+        customList[i],customList[0] = customList[0],customList[i]         
+        heapify(customList,i,0)
+    customList.reverse()      
     
 # test run
 cList = [9,8,7,6,5,4,3,2,1]
@@ -118,4 +176,6 @@ cList = [9,8,7,6,5,4,3,2,1]
 # selectionSort(cList)
 # insertionSort(cList)
 # print(bucketSort(cList))
-print(mergeSort(cList,0,8))
+# print(mergeSort(cList,0,8))
+heapSort(cList)
+print(cList)
